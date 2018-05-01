0df2e7c2be6e7a89baba1a517f79f71ac6c23fc7
john <jaselva@yahoo.com>
2018-05-01 19:24:41 -0400

    updated

--- src/test/java/com/orion/qa/testcases/Test_NewTimeSheet_SubmitFunctionality.java
+++ src/test/java/com/orion/qa/testcases/Test_NewTimeSheet_SubmitFunctionality.java
@@ -6,7 +6,7 @@ import static org.testng.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Level;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
@@ -47,6 +47,9 @@ public class Test_NewTimeSheet_SubmitFunctionality extends OrionBase {
 	/*		org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Test_NewTimeSheet_SubmitFunctionality.class);
 			logger.setLevel(Level.OFF);
 */
+			
+			PropertyConfigurator.configure(System.getProperty("user.dir") + "/src/main/resources/log4j.properties");
+			
 			init(Browser, true);
 
 
