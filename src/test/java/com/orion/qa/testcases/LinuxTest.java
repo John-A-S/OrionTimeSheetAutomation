1708ed0b59c2aa83822e6886bc1775e4de207881
john <jaselva@yahoo.com>
2018-05-02 11:39:31 -0400

    updated

--- src/test/java/com/orion/qa/testcases/LinuxTest.java
+++ src/test/java/com/orion/qa/testcases/LinuxTest.java
@@ -3,6 +3,7 @@ package com.orion.qa.testcases;
 import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,7 +19,11 @@ public class LinuxTest {
 	@BeforeMethod()
 	public void init() {
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		driver = new ChromeDriver();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary(chromeDriverPath);
+
+		driver = new ChromeDriver(options);
 		driver.get("http://www.google.com");
 	}
 
