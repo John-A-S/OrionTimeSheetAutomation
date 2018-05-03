7d5fb50e1166fb25d57cb02eeff755f213064db9
john <jaselva@yahoo.com>
2018-05-03 14:42:37 -0400

    Updated

--- src/test/java/com/orion/qa/testcases/LinuxTest.java
+++ src/test/java/com/orion/qa/testcases/LinuxTest.java
@@ -1,5 +1,7 @@
 package com.orion.qa.testcases;
 
+import static org.testng.Assert.assertEquals;
+
 import org.apache.logging.log4j.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,59 +13,47 @@ import org.testng.annotations.Test;
 
 public class LinuxTest {
 
-	// public static String chromeDriverPath = System.getProperty("user.dir")+"/src/main/input/chromedriver.exe";
-	//Below is for linux
-	//public static String chromeDriverPath = "/usr/bin/chromedriver";
-	public static String chromeDriverPath = System.getProperty("user.dir")+"/src/main/input/chromedriver";
-	
-	//public static String firefoxpath = System.getProperty("user.dir")+"/src/main/input/geckodriver.exe";
 	public WebDriver driver;
 
-	public static Logger log;
-	
+	// public static String chromeDriverPath =
+	// System.getProperty("user.dir")+"/src/main/input/chromedriver.exe";
+	// Below is for linux
+	// public static String chromeDriverPath = "/usr/bin/chromedriver";
+	public static String chromeDriverPath = System.getProperty("user.dir") + "/src/main/input/chromedriver";
+
+	// public static String firefoxpath =
+	// System.getProperty("user.dir")+"/src/main/input/geckodriver.exe";
+
 	@BeforeMethod()
 	public void init() {
+		/* Chrome driver */
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		
-		log = LogManager.getLogger(LinuxTest.class.getName());
 
-		System.out.println(LinuxTest.class.getName());
-		/*ChromeOptions options = new ChromeOptions();
-		options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");*/
+		ChromeOptions options = new ChromeOptions(); 
+		options.setBinary("/usr/bin/chromium/chrome");
 
-	//	driver = new ChromeDriver();
-	//	driver.get("http://www.google.com");
-	/*	System.setProperty("webdriver.firefox.marionette", firefoxpath);
-
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 
-	// System.out.println("Inside init - Before Method");*/	
+		/*
+		 * FireFox System.setProperty("webdriver.firefox.marionette", firefoxpath);
+		 * 
+		 * driver = new FirefoxDriver(); driver.get("http://www.google.com");
+		 * 
+		 * // System.out.println("Inside init - Before Method");
+		 */
 	}
 
 	@Test()
-	public void log4jMethod() {
-		log.debug("Debugging message");
-		log.info("Info Message");
-		log.error("error message");
-		log.warn("Warning message");
-		log.fatal("fatal message");
-		
-	}
-	
-	@Test()
-	public void verifyURL(){
-	/*	String title = driver.getTitle();
+	public void verifyURL() {
+		String title = driver.getTitle();
 		System.out.println(title);
 		assertEquals(title, "Google");
-	*/System.out.println("Inside verifyURL ");	
-	
-		}
-	
+	}
+
 	@AfterMethod()
 	public void closeBrowser() {
-//		driver.quit();
-		System.out.println("Inside close browser");
+		driver.quit();
 	}
 
 }
