80600019c1b9a25b7c386c00fe1538c01fd9e668
john <jaselva@yahoo.com>
2018-05-04 10:34:41 -0400

    Updated

--- src/test/java/com/orion/qa/testcases/LinuxTest.java
+++ src/test/java/com/orion/qa/testcases/LinuxTest.java
@@ -2,12 +2,25 @@ package com.orion.qa.testcases;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LinuxTest {
 
@@ -17,27 +30,62 @@ public class LinuxTest {
 
 	@BeforeMethod()
 	public void init() {
-		/* Chrome driver */
+		/* Chrome driver 
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 
-		ChromeOptions options = new ChromeOptions(); 
-		
-		 options.addArguments("--headless");
-		 options.addArguments("--no-sandbox");
-		 
-		 //	options.addArguments("--disable-extensions");
-		 //	options.addArguments("--disable-gpu");
-		 //	options.setBinary("/usr/bin/chromium");
+		ChromeOptions options = new ChromeOptions();
+
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+
+		// options.addArguments("--disable-extensions");
+		// options.addArguments("--disable-gpu");
+		// options.setBinary("/usr/bin/chromium");
 
 		driver = new ChromeDriver(options);
-		driver.get("http://www.google.com");
+		driver.get("http://www.google.com");*/
 	}
 
 	@Test()
 	public void verifyURL() {
-		String title = driver.getTitle();
+		/*String title = driver.getTitle();
 		System.out.println(title);
-		assertEquals(title, "Google");
+		assertEquals(title, "Google"); */
+	}
+
+	@Test()
+	public void downloadfile() throws ClientProtocolException, IOException {
+
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--test-type");
+		options.addArguments("--headless");
+		options.addArguments("--disable-extensions"); // to disable browser extension popup
+
+		ChromeDriverService driverService = ChromeDriverService.createDefaultService();
+		ChromeDriver driver = new ChromeDriver(driverService, options);
+
+		Map<String, Object> commandParams = new HashMap<String, Object>();
+		commandParams.put("cmd", "Page.setDownloadBehavior");
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("behavior", "allow");
+		params.put("downloadPath", chromeDriverPath);
+		commandParams.put("params", params);
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		
+		 HttpClient httpClient = HttpClientBuilder.create().build();
+         String command = objectMapper.writeValueAsString(commandParams);
+         String u = driverService.getUrl().toString() + "/session/" + driver.getSessionId() + "/chromium/send_command";
+         HttpPost request = new HttpPost(u);
+         request.addHeader("content-type", "application/json");
+         request.setEntity(new StringEntity(command));
+         httpClient.execute(request);
+
+         driver.get("http://www.seleniumhq.org/download/");
+		driver.findElement(By.linkText("32 bit Windows IE")).click();
+
 	}
 
 	@AfterMethod()
