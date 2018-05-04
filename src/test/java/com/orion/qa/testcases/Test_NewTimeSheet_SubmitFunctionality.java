481c788207874245f805e1f73d6c3972b270f022
john <jaselva@yahoo.com>
2018-05-04 12:19:48 -0400

    updated

--- src/test/java/com/orion/qa/testcases/Test_NewTimeSheet_SubmitFunctionality.java
+++ src/test/java/com/orion/qa/testcases/Test_NewTimeSheet_SubmitFunctionality.java
@@ -249,7 +249,16 @@ public class Test_NewTimeSheet_SubmitFunctionality extends OrionBase {
 
 			log.debug("setting Downloadproperties..");
 			setDownloadProperties();
+			
+			System.out.println(driver.toString());
+			
+	        driver.get("http://www.seleniumhq.org/download/");
+	        System.out.println("After Selenium dowload : "+ driver.toString());
+	        Thread.sleep(5000);
+			driver.findElement(By.linkText("32 bit Windows IE")).click();
 
+			
+/*
 			Thread.sleep(5000);
 			wait.until(ExpectedConditions.elementToBeClickable(By.linkText(TempFileName)));
 			System.out.println("Temp file name :" + TempFileName);
@@ -267,7 +276,7 @@ public class Test_NewTimeSheet_SubmitFunctionality extends OrionBase {
 			} else {
 				isSameFiles = false;
 				log.info("File comparison completed successfully.  Both files mis-matches");
-			}
+			}*/
 		} catch (Exception e) {
 			e.printStackTrace();
 			log.error("Exception in method DownloadfileAndComparewithTestFile "+e.getMessage());
