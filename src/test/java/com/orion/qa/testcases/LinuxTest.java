3cc91f727d00565a73cde8388b8ab13eb9b32d2d
john <jaselva@yahoo.com>
2018-05-10 13:37:41 -0400

    Updated

--- src/test/java/com/orion/qa/testcases/LinuxTest.java
+++ src/test/java/com/orion/qa/testcases/LinuxTest.java
@@ -94,8 +94,8 @@ public class LinuxTest {
 
 		System.out.println("Before get" + driver.toString());
 		
-		DownloadDocfromOrion();
-		//DownloadDocfromExternal();
+		//DownloadDocfromOrion();
+		DownloadDocfromExternal();
 		
 	}
 
@@ -176,7 +176,7 @@ public class LinuxTest {
 		// clickAndSaveFileIE(driver.findElement(By.linkText("Free download")));
 
 		Actions act1 = new Actions(driver);
-		WebElement ele = driver.findElement(By.xpath("//a[contains(text(), 'Free download')]"));
+		WebElement ele = driver.findElement(By.xpath("//a[contains(text(), 'Free download1')]"));
 		act1.moveToElement(ele).click().build().perform();
 		
 
